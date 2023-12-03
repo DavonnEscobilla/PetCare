d98f1728e7d2f21666f059bc607fc6ccca0e7bfe
lexiaroque <alexiarchives@gmail.com>
2023-12-04 01:49:56 +0800

    MedicalScreen with Pet

--- components/MedicalHomeScreen.js
+++ components/MedicalHomeScreen.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import { View, Text, TouchableHighlight, StyleSheet, TouchableOpacity } from 'react-native';
-import { AntDesign } from '@expo/vector-icons'; // Import the back arrow icon
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  Image,
+  FlatList,
+  StyleSheet,
+  TouchableHighlight,
+} from 'react-native';
+import { auth, database } from '../firebase';
+import { ref, onValue, off } from 'firebase/database';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { AntDesign } from '@expo/vector-icons'; // Import the back arrow icon
 import RecordListScreen from './RecordListScreen';
 
+
 const MedicalHomeScreen = ({ navigation }) => {
-  const navigateToScreen = (screenName, params) => {
-    navigation.navigate(screenName, params);
-  };
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    if (!auth.currentUser) {
+      console.error('No user logged in');
+      return;
+    }
+
+    const userId = auth.currentUser.uid;
+    const userPetsRef = ref(database, `users/${userId}/pets`);
+
+    const unsubscribe = onValue(userPetsRef, (snapshot) => {
+      const data = snapshot.val();
+      const petsArray = data
+        ? Object.keys(data).map((key) => ({
+            id: key,
+            ...data[key],
+          }))
+        : [];
+      setPets(petsArray);
+    }, (error) => {
+      console.error(error);
+    });
+
+    return () => off(userPetsRef);
+  }, []);
+
+  const renderPet = ({ item }) => (
+    <View style={styles.petImageContainer}>
+      <Image source={{ uri: item.image }} style={styles.petImage} />
+      <Text style={styles.petName}>{item.name}</Text>
+    </View>
+  );
 
   return (
     <View style={styles.container}>
@@ -21,31 +61,23 @@ const MedicalHomeScreen = ({ navigation }) => {
         </TouchableHighlight>
         <Text style={styles.headerTitle}>Pet Health</Text>
       </View>
-      
+
+      {/* Pet Images Container */}
       <View style={styles.petContainer}>
-        <Text style={styles.petTitle}>My Pets</Text>
-        <View style={styles.petImagesContainer}>
-          {/* Images of pets */}
-        </View>
+      <Text style={styles.petTitle}>My Pets</Text>
+      <View style={styles.petImagesContainer}>
+        <FlatList
+          data={pets}
+          renderItem={renderPet}
+          keyExtractor={(item) => item.id}
+          horizontal={true}
+          
+        />
       </View>
-
-
-      <View style={styles.vaccineContainer}>
-        <Text style={styles.vaccineTitle}>Vaccinations</Text>
-        <View style={styles.vaccineInfoContainer}>
-          {/* Images of pets */}
-        </View>
-      </View>
-
-      <View style={styles.groomingContainer}>
-        <Text style={styles.groomingTitle}>Grooming</Text>
-        <View style={styles.groomingInfoContainer}>
-          {/* Images of pets */}
-        </View>
       </View>
 
       {/* Content */}
-      <RecordListScreen navigateToScreen={navigateToScreen} />
+      <RecordListScreen navigateToScreen={navigation} />
 
       {/* Add Record Button */}
       <TouchableHighlight
@@ -54,10 +86,7 @@ const MedicalHomeScreen = ({ navigation }) => {
       >
         <Text style={styles.buttonText}>Add Record</Text>
       </TouchableHighlight>
-    
-
     </View>
-
   );
 };
 
@@ -73,23 +102,54 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     paddingLeft: 0.6, // Adjust this to your liking for left padding
-    
   },
   backButton: {
-      position: 'absolute',
-      top: 35,
-      left: 25,
-      padding: 15,
-      borderRadius: 0,
-      backgroundColor: '#D14E86',
-      alignSelf: 'flex-start',
-    },
+    position: 'absolute',
+    top: 35,
+    left: 25,
+    padding: 15,
+    borderRadius: 0,
+    backgroundColor: '#D14E86',
+    alignSelf: 'flex-start',
+  },
   headerTitle: {
     color: 'white',
     fontSize: 22,
     fontWeight: 'bold',
     alignSelf: 'center',
     marginTop: 30,
+  },
+  petContainer: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    height: 150,
+    width: '90%',
+    alignSelf: 'center',
+    marginTop: 20,
+    shadowColor: '#000', // Shadow color
+    shadowOffset: { width: 0, height: 2 }, // Shadow offset for x and y
+    shadowOpacity: 0.3, // Shadow opacity
+    shadowRadius: 5, // Shadow blur radius
+    elevation: 1, // Use elevation for Android
+  },
+  petImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 50,
+    marginHorizontal: 10,
+    alignSelf: 'center',
+  },
+  petImagesContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  petName: {
+    flex: 1,
+    fontSize: 15,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   addButton: {
     backgroundColor: '#D14E86',
