613a235b0fb70449f40c121e6b226bb6b8e3ab5c
pmlmagat <qpmgmagat@tip.edu.ph>
2023-12-03 18:20:55 +0800

    User profile and user name connected to backend. Editable username

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,75 +1,140 @@
-import PetProfile  from './PetProfile';
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image, Alert } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import * as ImagePicker from 'expo-image-picker';
+import PetProfile from "./PetProfile";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  Alert,
+  TextInput,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import * as ImagePicker from "expo-image-picker";
+import { auth, database } from "../firebase";
+import { ref, set, onValue } from "firebase/database";
+import { signOut } from "firebase/auth";
+
+const saveUserData = async (newUserName, profilePicUrl) => {
+  if (auth.currentUser) {
+    const userId = auth.currentUser.uid;
+    const userRef = ref(database, "users/" + userId);
+    try {
+      await set(userRef, {
+        username: newUserName,
+        profilePicture: profilePicUrl,
+      });
+    } catch (error) {
+      console.error("Error saving user data: ", error);
+      Alert.alert("Error", "Failed to save user data.");
+    }
+  }
+};
 
 const UserProfile = ({ navigation }) => {
-  // Add these state hooks
-  const [file, setFile] = useState(null); 
-  const [error, setError] = useState(null);
+  const [userName, setUserName] = useState("Default Name");
+  const [profilePic, setProfilePic] = useState(null);
+  const [file, setFile] = useState(null);
+
+  useEffect(() => {
+    if (auth.currentUser) {
+      const userId = auth.currentUser.uid;
+      const userRef = ref(database, "users/" + userId);
+      onValue(userRef, (snapshot) => {
+        const data = snapshot.val();
+        if (data) {
+          setUserName(data.username);
+          setProfilePic(data.profilePicture);
+        }
+      });
+    }
+  }, []);
 
   const handleSignOut = () => {
-    console.log('Signing out...');
+    signOut(auth)
+      .then(() => {
+        console.log("User signed out!");
+        setUserName("Default Name"); // Reset username state
+        setProfilePic(null); // Reset profile picture state
+        navigation.navigate("Intro"); // Navigate to intro or login screen
+      })
+      .catch((error) => {
+        console.error("Error signing out: ", error);
+        Alert.alert("Error", "Problem signing out.");
+      });
   };
 
   const handleAddProfile = () => {
-    console.log('Add a new pet profile...');
+    console.log("Add a new pet profile...");
   };
 
   const pickImage = async () => {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (status !== 'granted') {
-      Alert.alert('Permission Denied', `Sorry, we need camera roll permissions to upload images.`);
-      return;
+    if (status !== "granted") {
+        Alert.alert(
+            "Permission Denied",
+            `Sorry, we need camera roll permissions to upload images.`
+        );
+        return; // Early return if permission is not granted
     }
-  
+
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true, // If you want to allow editing
-      aspect: [4, 3], // Aspect ratio
-      quality: 1, // Keep the quality high
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true, // If you want to allow editing
+        aspect: [4, 3], // Aspect ratio
+        quality: 1, // Keep the quality high
     });
-  
+
     if (!result.cancelled) {
-      // Setting the image URI
-      setFile(result.assets[0].uri);
+        setFile(result.assets[0].uri);
+        // Save user data to Firebase
+        saveUserData(userName, result.assets[0].uri); // Pass the current userName
     }
-  };
+};
 
-return (
+
+  return (
     <View style={styles.homeContainer}>
-
       {/* Header Container */}
       <View style={styles.headerContainer}>
-      <TouchableOpacity
+        <TouchableOpacity
           style={styles.backtoHome}
-          onPress={() => navigation.navigate('Home')} 
-          >
+          onPress={() => navigation.navigate("Home")}
+        >
           <Icon name="arrow-left" size={30} color="#D14E86" />
         </TouchableOpacity>
       </View>
 
       {/* UserProfile Container */}
       <View style={styles.userProfileContainer}>
-        <TouchableOpacity
-          style={styles.userPhotoContainer}
-          onPress={pickImage}
-        >
-          <Image 
-            source={file ? { uri: file } : require('../assets/defaultProfile.png')} 
+        <TouchableOpacity style={styles.userPhotoContainer} onPress={pickImage}>
+          <Image
+            source={
+              profilePic
+                ? { uri: profilePic }
+                : require("../assets/defaultProfile.png")
+            }
             style={styles.userPhoto}
           />
-          <Icon name="camera" size={24} color="gray" style={styles.cameraIcon} />
+          <Icon
+            name="camera"
+            size={24}
+            color="gray"
+            style={styles.cameraIcon}
+          />
         </TouchableOpacity>
-        
 
         {/* User's name */}
-        <Text style={styles.userName}>{'Jane Emerson'}</Text>
+        <TextInput
+          style={styles.userNameInput}
+          value={userName}
+          onChangeText={setUserName}
+          onEndEditing={() => saveUserData(userName, profilePic || "")}
+        />
 
         <TouchableOpacity
           style={styles.logoutButton}
-          onPress={() => navigation.navigate('Intro')}
+          onPress={() => navigation.navigate("Intro")}
         >
           <Text style={styles.logoutText}>Logout</Text>
         </TouchableOpacity>
@@ -79,7 +144,7 @@ return (
       <View style={styles.petProfileContainer}>
         <TouchableOpacity
           style={styles.addPetButton}
-          onPress={() => navigation.navigate('PetInput')}
+          onPress={() => navigation.navigate("PetInput")}
         >
           <Icon name="plus-circle" size={30} color="black" />
         </TouchableOpacity>
@@ -89,21 +154,21 @@ return (
       <View style={styles.buttonContainer}>
         <TouchableOpacity
           style={styles.button}
-          onPress={() => navigation.navigate('Home')} 
-          >
+          onPress={() => navigation.navigate("Home")}
+        >
           <Icon name="home" size={30} color="black" />
         </TouchableOpacity>
 
         <TouchableOpacity
           style={styles.button}
-          onPress={() => navigation.navigate('MagnifyingGlassDog')}
+          onPress={() => navigation.navigate("MagnifyingGlassDog")}
         >
           <Icon name="search" size={30} color="black" />
         </TouchableOpacity>
 
         <TouchableOpacity
           style={styles.button}
-          onPress={() => navigation.navigate('Calendar')}
+          onPress={() => navigation.navigate("Calendar")}
         >
           <Icon name="calendar" size={30} color="black" />
         </TouchableOpacity>
@@ -117,21 +182,21 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   headerContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     // backgroundColor: 'yellow',
     paddingTop: 50,
     paddingLeft: 20,
   },
   userProfileContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   userPhotoContainer: {
     width: 150,
     height: 150,
-    backgroundColor: 'lightgray',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "lightgray",
+    justifyContent: "center",
+    alignItems: "center",
     borderRadius: 10,
   },
   userPhoto: {
@@ -140,45 +205,48 @@ const styles = StyleSheet.create({
     borderRadius: 10,
   },
   cameraIcon: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 8,
     right: 8,
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
   },
-  userName: {
+  userNameInput: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+    textAlign: "center",
+    // borderBottomWidth: 1,
+    borderBottomColor: "gray", // You can adjust the color
+    marginVertical: 5,
   },
   petProfileContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 20,
   },
   logoutText: {
     fontSize: 14,
-    color: 'gray',
-    textDecorationLine: 'underline',
+    color: "gray",
+    textDecorationLine: "underline",
     paddingHorizontal: 10,
-    textAlign: 'center',
+    textAlign: "center",
   },
-  logoutBotton:{
+  logoutBotton: {
     // top: 100, // Corrected property name to lowercase 'top'
   },
   addPetIcon: {
-    backgroundColor: 'transparent',
-
+    backgroundColor: "transparent",
   },
   buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
     // backgroundColor: '#fff8dc',
-    position: 'absolute',
+    position: "absolute",
     bottom: 0,
-    width: '100%',
+    width: "100%",
   },
   button: {
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
     paddingVertical: 5,
     paddingHorizontal: 20,
     borderRadius: 5,
